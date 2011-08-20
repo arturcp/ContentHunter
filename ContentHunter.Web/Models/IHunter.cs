c5f654eb2247ae0d1d65f9ff8fc580bbe2c390a4
Artur <arturbcc@yahoo.com.br>
2011-08-19 23:04:19 -0300

    preparando interface para metodos reais

--- ContentHunter.Web/Models/IHunter.cs
+++ ContentHunter.Web/Models/IHunter.cs
@@ -8,5 +8,8 @@ namespace ContentHunter.Web.Models
     public interface IHunter
     {
         string Parse(string url);
+        //List<string> CandidatesToRecursion(string url);
+        //string ParseHtml(string url);
+        //string ParseRss(string url);
     }
 }
